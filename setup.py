37e867d7f139edd8716f5a7e60919bdd4bcbac9b
Jack Diederich <jack@hivefire.com>
2011-06-02 16:22:45 -0400

    reorg files and create a setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import distutils.core
+distutils.core.setup(
+    name='textcatcher',
+    description='multiline text matcher library',
+    author='Jack Diederich',
+    author_email='jackdied@gmail.com',
+    url='http://github.com/jackdied/textcatcher',
+    py_modules=['textcatcher'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        ]
+   )
+
+
